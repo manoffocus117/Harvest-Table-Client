5b9e04a3bebc04e11f5d9e26ac5c99713a277d7d
manoffocus117 <saifurrahmansaif954@gmail.com>
2026-09-06 00:53:44 +0600

    footer section added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
+import {
+      RiFacebookCircleFill,
+      RiInstagramLine,
+      RiTwitterFill,
+} from "@remixicon/react";
 import React from "react";
 
 const Footer = () => {
-      return <footer>Footer</footer>;
+      return (
+            <footer className="flex flex-col sm:flex-row items-start">
+                  <div className="bg-[#1F2937] w-full md:w-1/2 py-20 text-white flex flex-col items-center justify-center gap-3">
+                        <h3 className="text-3xl mb-3">Contact Us</h3>
+                        <p>Somewhere in Bangladesh</p>
+                        <p>+88 01234567890</p>
+                        <p>Sat - Thus: 8:00AM - 10:00PM</p>
+                        <p>Fri : Close</p>
+                  </div>
+                  <div className="bg-[#111827] w-full md:w-1/2 pt-20 pb-24 text-white flex flex-col items-center justify-center gap-10">
+                        <h3 className="text-3xl mb-3">Follow Us</h3>
+                        <p>Join us on social media</p>
+                        <figure className="flex items-center justify-center gap-10">
+                              <RiFacebookCircleFill />
+                              <RiInstagramLine />
+                              <RiTwitterFill />
+                        </figure>
+                  </div>
+            </footer>
+      );
 };
 
 export default Footer;
