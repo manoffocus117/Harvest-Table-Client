b4182c1c01dfb8a8a925838db2eb8381e3ce4539
manoffocus117 <saifurrahmansaif954@gmail.com>
2026-09-07 20:47:00 +0600

    login form added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,64 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import Login_bg from "../assets/reservation/wood-grain-pattern-gray1x.png";
+import Login_side_image from "../assets/others/authentication2.png";
 
 const Login = () => {
       return (
-            <section className="h-screen">
+            <section
+                  className="shadow-xl rounded-xl flex flex-col md:flex-row items-center justify-between gap-20 md:gap-0 p-5 md:p-10"
+                  style={{ backgroundImage: `url(${Login_bg})` }}
+            >
                   <Helmet>
                         <title>Harvest Table | Login</title>
                   </Helmet>
-                  <h1 className="text-5xl">Login</h1>
+                  <div className="w-full md:w-8/12 flex flex-row justify-end">
+                        <figure>
+                              <img src={Login_side_image} alt="" />
+                        </figure>
+                  </div>
+                  <div className="w-full md:w-6/12 bg-white p-5 md:p-10 md:mx-30 md:my-20 rounded-xl">
+                        <h1 className="text-4xl text-center mb-10">Login</h1>
+                        <form className="fieldset gap-5">
+                              <fieldset className="fieldset">
+                                    <label htmlFor="email">Email</label>
+                                    <input
+                                          type="email"
+                                          id="email"
+                                          className="input outline-none w-full"
+                                          placeholder="Email"
+                                    />
+                              </fieldset>
+                              <fieldset className="fieldset">
+                                    <label htmlFor="password">Password</label>
+                                    <input
+                                          type="password"
+                                          id="password"
+                                          className="input outline-none w-full"
+                                          placeholder="Password"
+                                    />
+                              </fieldset>
+                              <fieldset className="fieldset">
+                                    <label htmlFor="recaptcha">Recaptcha</label>
+                                    <input
+                                          type="text"
+                                          id="recaptcha"
+                                          className="input outline-none w-full"
+                                          placeholder="Recaptcha"
+                                    />
+                              </fieldset>
+                              <fieldset className="fieldset">
+                                    <input
+                                          type="text"
+                                          className="input outline-none w-full"
+                                          placeholder="Enter Recaptcha"
+                                    />
+                              </fieldset>
+                              <button type="submit" className="btn btn-primary">
+                                    Sign In
+                              </button>
+                        </form>
+                  </div>
             </section>
       );
 };
