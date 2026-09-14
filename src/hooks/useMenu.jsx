61018aee024d01576b43b50a2880ef3a4f38330b
manoffocus <saifurrahmansaif954@gmail.com>
2026-09-14 22:32:14 +0600

    custom hook added to load menu data

---
+++ src/hooks/useMenu.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const useMenu = () => {
+      // state for menu
+      const [menu, set_menu] = useState([]);
+
+      // state for loader
+      const [loading, set_loading] = useState(true);
+      // loading menu data
+      useEffect(() => {
+            fetch("menu.json")
+                  .then((res) => res.json())
+                  .then((data) => {
+                        set_menu(data);
+                        set_loading(false);
+                  });
+      }, []);
+      return [menu, loading];
+};
+
+export default useMenu;
--- src/pages/home/sections/Menu.jsx
+++ src/pages/home/sections/Menu.jsx
@@ -1,28 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Title from "./../../../components/Title";
 import Menu_item from "../../../components/Menu_item";
 import Button from "../../../components/Button";
+import useMenu from "../../../hooks/useMenu";
 
 const Menu = () => {
-      // state for menu
-      const [menu, set_menu] = useState([]);
-      // loading menu data
-      useEffect(() => {
-            fetch("menu.json")
-                  .then((res) => res.json())
-                  .then((data) => {
-                        const popular_items = data.filter(
-                              (item) => item.category === "popular",
-                        );
-                        set_menu(popular_items);
-                  });
-      }, []);
-
+      const [menu] = useMenu();
+      const popular_item = menu.filter((item) => item.category === "popular");
       return (
             <section className="flex flex-col items-center">
                   <Title sub_title={"Check it out"} title={"Form our menu"} />
                   <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mb-10">
-                        {menu.map((item) => (
+                        {popular_item.map((item) => (
                               <Menu_item key={item._id} item={item} />
                         ))}
                   </div>
