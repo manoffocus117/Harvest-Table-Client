30b135c71b12049b7f79471ce3837ba676929346
manoffocus <saifurrahmansaif954@gmail.com>
2026-09-21 23:46:11 +0600

    header modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -88,39 +88,40 @@ const Header = () => {
                                     {navlink}
                               </menu>
                         </div>
-                        <div className="navbar-end gap-3 lg:gap-0">
-                              {/* user pri=ofile & shopping cart */}
-                              <div className="flex gap-3">
-                                    {/* shopping cart */}
-                                    <div className="dropdown dropdown-end">
-                                          <div
-                                                tabIndex={0}
-                                                role="button"
-                                                className="btn btn-ghost btn-circle bg-base-300"
-                                          >
-                                                <RiShoppingCart2Line />
-                                          </div>
-                                          <div
-                                                tabIndex={0}
-                                                className="card card-sm dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
-                                          >
-                                                <div className="card-body">
-                                                      <span className="text-lg font-bold">
-                                                            8 Items
-                                                      </span>
-                                                      <span className="text-info">
-                                                            Subtotal: $999
-                                                      </span>
-                                                      <div className="card-actions">
-                                                            <button className="btn btn-primary btn-block">
-                                                                  View cart
-                                                            </button>
+                        {user && (
+                              <div className="navbar-end gap-3 lg:gap-0">
+                                    {/* user profile & shopping cart */}
+                                    <div className="flex gap-3">
+                                          {/* shopping cart */}
+                                          <div className="dropdown dropdown-end">
+                                                <div
+                                                      tabIndex={0}
+                                                      role="button"
+                                                      className="btn btn-ghost btn-circle bg-base-300"
+                                                >
+                                                      <RiShoppingCart2Line />
+                                                </div>
+                                                <div
+                                                      tabIndex={0}
+                                                      className="card card-sm dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
+                                                >
+                                                      <div className="card-body">
+                                                            <span className="text-lg font-bold">
+                                                                  8 Items
+                                                            </span>
+                                                            <span className="text-info">
+                                                                  Subtotal: $999
+                                                            </span>
+                                                            <div className="card-actions">
+                                                                  <button className="btn btn-primary btn-block">
+                                                                        View
+                                                                        cart
+                                                                  </button>
+                                                            </div>
                                                       </div>
                                                 </div>
                                           </div>
-                                    </div>
-                                    {/* user profile */}
-                                    {user && (
+                                          {/* user profile */}
                                           <div className="dropdown dropdown-end">
                                                 <div
                                                       tabIndex={0}
@@ -157,25 +158,25 @@ const Header = () => {
                                                       </li>
                                                 </menu>
                                           </div>
-                                    )}
-                              </div>
-                              {/* mobile nav menu */}
-                              <div className="dropdown dropdown-end lg:hidden">
-                                    <div
-                                          tabIndex={0}
-                                          role="button"
-                                          className="btn btn-ghost btn-circle bg-base-300"
-                                    >
-                                          <RiMenu5Line className="rounded-full" />
                                     </div>
-                                    <menu
-                                          tabIndex={-1}
-                                          className="menu menu-sm gap-3 dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-                                    >
-                                          {navlink}
-                                    </menu>
+                                    {/* mobile nav menu */}
+                                    <div className="dropdown dropdown-end lg:hidden">
+                                          <div
+                                                tabIndex={0}
+                                                role="button"
+                                                className="btn btn-ghost btn-circle bg-base-300"
+                                          >
+                                                <RiMenu5Line className="rounded-full" />
+                                          </div>
+                                          <menu
+                                                tabIndex={-1}
+                                                className="menu menu-sm gap-3 dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                                          >
+                                                {navlink}
+                                          </menu>
+                                    </div>
                               </div>
-                        </div>
+                        )}
                   </div>
             </header>
       );
