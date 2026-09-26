2c62dd35148945d3f416ca6fa64de08df84865fd
manoffocus <saifurrahmansaif954@gmail.com>
2026-09-26 21:32:28 +0600

    forcing user to login before adding item to the cart

--- src/components/Product_card.jsx
+++ src/components/Product_card.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
+import Auth_context from "./../context/Auth_context";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router";
 
 const Product_card = ({ item }) => {
+      const { user } = useContext(Auth_context);
       const { name, image, recipe, price } = item;
+      const navigate = useNavigate();
+      const location = useLocation();
 
       // handler for add to cart
       const handle_add_to_cart = (food_item) => {
             console.log(food_item);
+            if (user && user.email) {
+                  // send cart item to the database
+                  Swal.fire({
+                        icon: "success",
+                        title: "Success",
+                        text: "Your item is added to the cart",
+                        confirmButtonColor: "rgb(251, 170, 0)",
+                  });
+            } else {
+                  Swal.fire({
+                        title: "Not found",
+                        text: "You are not logged in. Please login",
+                        icon: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "rgb(251, 170, 0)",
+                        cancelButtonColor: "#d33",
+                        confirmButtonText: "Take to the login page",
+                  }).then((result) => {
+                        if (result.isConfirmed)
+                              navigate("/login", {
+                                    state: { from: location },
+                                    replace: true,
+                              });
+                  });
+            }
       };
       return (
             <div className="card bg-base-100 shadow-sm hover:scale-105">
